2dac2da002dd4e3c86b0d8c442792ff8fccb1dc0
honda <bentianhuangtailang@gmail.com>
2025-05-03 23:31:52 +0900

    loginのファイルに/api/を追加

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -23,7 +23,7 @@ export default function Login() {
     e.preventDefault();
 
     try {
-      const res = await fetch("http://localhost:8000/login", {
+      const res = await fetch("http://localhost:8000/api/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
