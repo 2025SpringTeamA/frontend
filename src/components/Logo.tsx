e15e57681b5f09aa0ed42d5feec54b25b18e25fd
mo-ri-regen <moricontb@gmail.com>
2025-05-05 23:46:03 +0900

    ロゴを余白がない画像に変更

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -6,9 +6,18 @@ import Image from "next/image";
 export default function Logo() {
   return (
     <>
-      <Link href="/home">
-        <Image src="/logo.svg" alt="ロゴ" width={100} height={100}></Image>
-      </Link>
+      <header className="w-full p-4">
+        <div className="flex items-start justify-start m-0 p-0">
+          <Link href="/home">
+            <Image
+              src="/images/sabuchan_logo.png"
+              alt="ロゴ"
+              width={100}
+              height={100}
+            ></Image>
+          </Link>
+        </div>
+      </header>
     </>
   );
 }
