d139b48e75cda82f879bd1ac353e233f39b05b36
mo-ri-regen <moricontb@gmail.com>
2025-05-05 23:43:27 +0900

    マイページ画面　CSS修正

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -23,40 +23,44 @@ export default function Home() {
     <>
       <Logo />
       <NavBar />
+      <div className="flex flex-col items-center space-y-4">
+        <div className="flex flex-col space-y-1 w-full max-w-sm">
+          <label htmlFor="username" className="font-semibold">
+            ユーザ名
+          </label>
+          <Input
+            type="text"
+            placeholder="ユーザ名"
+            name="text"
+            className="bg-white border px-3 py-2 rounded"
+          />
+        </div>
+        <div className="flex flex-col space-y-1 w-full max-w-sm">
+          <label htmlFor="password" className="font-semibold">
+            パスワード
+          </label>
+          <Input
+            type="password"
+            placeholder="パスワード"
+            name="password"
+            className="bg-white border px-3 py-2 rounded"
+          />
+        </div>
+        <div className="flex items-center space-x-4">
+          <Link
+            href="/mypage-edit"
+            className="bg-gray-200 text-black px-4 py-2 rounded hover:bg-gray-300 text-center"
+          >
+            変更
+          </Link>
 
-      <div className="flex flex-col items-center">
-        {/* <form> */}
-        <label>ユーザ名</label>
-        <input
-          type="text"
-          placeholder="ユーザ名"
-          name="text"
-          style={{ background: "white", color: "black" }}
-        />
-      </div>
-      <div className="flex flex-col items-center">
-        <label>パスワード</label>
-        <input
-          type="password"
-          placeholder="パスワード"
-          name="password"
-          style={{ background: "white", color: "black" }}
-        />
-      </div>
-      <div className="flex items-center space-x-4">
-        <Link
-          href="/mypage-edit"
-          className="bg-gray-200 text-black px-4 py-2 rounded hover:bg-gray-300 w-[140px]"
-        >
-          変更
-        </Link>
-
-        <Button
-          className="bg-gray-200 text-black px-4 py-2 rounded hover:bg-gray-300"
-          onClick={handleDeleteClick}
-        >
-          削除
-        </Button>
+          <Button
+            className="bg-gray-200 text-black px-4 py-2 rounded hover:bg-gray-300"
+            onClick={handleDeleteClick}
+          >
+            削除
+          </Button>
+        </div>
       </div>
     </>
   );
