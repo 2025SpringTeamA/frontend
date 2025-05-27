1369870bc49d3eb0a8a2bc1de415d5c3f9201035
honda <bentianhuangtailang@gmail.com>
2025-05-27 21:51:17 +0900

    fix: localhost をバックエンドドメインに変更

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -23,7 +23,7 @@ const Register: React.FC = () => {
     e.preventDefault();
 
     try {
-      const res = await fetch("http://localhost:8000/api/register", {
+      const res = await fetch("http://api.saburo.xyz:8000/api/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
